548375f3e55ef0d15354ff796e4137a9e14c4fa3
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-09 15:53:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
-$server = 'DESKTOP-5OQ88VR\MSSQL2014';
 
-// Connect to MSSQL
-$link = mssql_connect($server);
+$server = "DESKTOP-5OQ88VR\MSSQL2014";
+$user = "DESKTOP-5OQ88VR\Kasco";
+$pass = "";
+$db_name="sport"; 
 
-if (!$link) {
-    echo'Something went wrong while connecting to MSSQL';
-}
+//connection to database
+$dbhandle = mssql_connect($server, $user, $pass) or die ("Cannot connect to Server");
+
 
