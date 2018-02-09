874a39c76f71928126166a6d245b2b21bfb8631c
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-09 15:05:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,6 @@ if( $conn ) {
      echo "Connection established.<br />";
 }else{
      echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
+     //die( print_r( sqlsrv_errors(), true));
 }
 
