186ec714472c6c13920ac29cc6e03f99e7124edb
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-09 17:32:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,22 @@
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
 
-$server = "DESKTOP-5OQ88VR\MSSQL2014";
-$user = "DESKTOP-5OQ88VR\Kasco";
-$pass = "";
-$db_name="sport"; 
-try {
-  //connection to database
-$dbhandle = mssql_connect($server, $user, $pass) or die ("Cannot connect to Server");
+$serverName = "DESKTOP-5OQ88VR\MSSQL2014";
+$uid = "DESKTOP-5OQ88VR\Kasco";
+$pwd = "";
+$dbase="sport"; 
+/* 
+ * Establish a Connection to the SQL Server 
+ */ 
 
-} catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+/* Define the Connection String with an array */
+$connectionInfo = array( "Database"=>$dbase, "UID"=>$uid, "PWD"=>$pwd);
+/* Open the connection */
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+/* Catch and display any connection error message */
+if( $conn === false ) {
+     echo "Unable to connect.</br>";
+     die( print_r( sqlsrv_errors(), true));
 }
 
 
