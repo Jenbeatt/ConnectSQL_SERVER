24c7d51903d49e4a92259ef6784a34aa18dc339e
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-09 15:56:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,13 @@ $server = "DESKTOP-5OQ88VR\MSSQL2014";
 $user = "DESKTOP-5OQ88VR\Kasco";
 $pass = "";
 $db_name="sport"; 
+try {
+  //connection to database
+$dbhandle = mssql_connect($server, $user, $pass) or die ("Cannot connect to Server");
 
-//connection to database
-//$dbhandle = mssql_connect($server, $user, $pass) or die ("Cannot connect to Server");
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
 
 
