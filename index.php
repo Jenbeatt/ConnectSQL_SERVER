a1e07eadbfffc89897ae4ca465c5aaa68ff5923e
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-09 17:34:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,8 @@ $conn = sqlsrv_connect( $serverName, $connectionInfo);
 if( $conn === false ) {
      echo "Unable to connect.</br>";
      die( print_r( sqlsrv_errors(), true));
-}
+}else
+{echo "Connect Success !!";}
 
 
 
