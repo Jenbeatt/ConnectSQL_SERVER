326006264cc5c6e25c589437c0d0ea4635f1769b
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-09 15:09:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
-$serverName = "DESKTOP-5OQ88VR\MSSQL2014"; //serverName\instanceName
-
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
-$connectionInfo = array( "Database"=>"sports");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+$server = 'DESKTOP-5OQ88VR\MSSQL2014';
 
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
-     //die( print_r( sqlsrv_errors(), true));
+// Connect to MSSQL
+$link = mssql_connect($server);
+
+if (!$link) {
+    die('Something went wrong while connecting to MSSQL');
 }
 
