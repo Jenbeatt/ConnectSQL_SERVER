9ee1e7fcd22e5efb8126450b697626139b00e14d
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-10 20:31:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php
 
 error_reporting(-1);
-
 ini_set('display_errors', 'On');
-
 ini_set("display_errors",1);
 ini_set("error_reporting",E_ALL);
-
-
 date_default_timezone_set("Asia/Bangkok");
-$host="localhost";
-$username="root";
-$password="";
-$dbname="dahangconner";
-$conn = new mysqli($host, $username, $password,$dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "1234";
+ $db = "dahangconner";
+
+
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
 
 
 
