31a2bf8417f572795baf7d99b5be8a70ef874232
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-10 14:51:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,43 @@
 <?php
-// Since UID and PWD are not specified in the $connectionInfo array,
-// The connection will be attempted using Windows Authentication.
 
-$serverName = "DESKTOP-5OQ88VR\MSSQL2014";
-$uid = "DESKTOP-5OQ88VR\Kasco";
-$pwd = "";
-$dbase="sport"; 
+error_reporting(-1);
 
-resource mssql_connect ([ string $serverName [, string $uid [, string $pwd [, bool $new_link = FALSE ]]]] )
-/* Define the Connection String with an array */
-//$connectionInfo = array( "Database"=>$dbase, "UID"=>$uid, "PWD"=>$pwd);
-/* Open the connection */
-//$conn = mssql_connect( $serverName, $connectionInfo);
-/* Catch and display any connection error message */
-//if( $conn === false ) {
-     echo "Unable to connect.</br>";
-   //  die( print_r( sqlsrv_errors(), true));
-//}else
-//{echo "Connect Success !!";}
+ini_set('display_errors', 'On');
+
+ini_set("display_errors",1);
+ini_set("error_reporting",E_ALL);
+
+$conn = mssql_connect('localhost','DESKTOP-5OQ88VR\Kasco', ''); 
+
+mssql_select_db('sport', $conn);
+
+$result = mssql_query("select * from sport", $conn);
+
+if(!$result){
+
+	var_dump(mssql_get_last_message());
+
+
+	die('MSSQL error: ' . mssql_get_last_message());
+}
+
+if($result === true){
+
+	var_dump('result', $result);
+	die;
+}
+
+
+$rows = mssql_fetch_assoc($result);
+
+if(!$rows){
+
+	die('MSSQL error: ' . mssql_get_last_message());
+
+}else{
+
+	var_dump($rows);
+}
 
 
 
