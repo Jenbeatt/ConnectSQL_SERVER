0ae542b2f9fa5214060189ef78a8d99a84332a3f
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-11 16:07:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
+
 <?php
-
-error_reporting(-1);
-ini_set('display_errors', 'On');
-ini_set("display_errors",1);
-ini_set("error_reporting",E_ALL);
-date_default_timezone_set("Asia/Bangkok");
-
- 
-$conn = mysqli_connect("ballbib.000webhostapp.com", "id4708578_test1234","test1234", "id4708578_test1234") or die(mysql_error());
-
-
+$conn = mysqli_connect("localhost","id4708578_test1234","test1234","id4708578__ballbib");
+if(!$conn){
+	echo "Not Connnect DatatBase";
+}else{
+	echo "Connnect Success";
+	
+}
 
 
 
