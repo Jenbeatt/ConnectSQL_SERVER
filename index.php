322f18852b304d57a393806ca2e698b1b338098e
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-02-11 15:32:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,8 @@ ini_set("display_errors",1);
 ini_set("error_reporting",E_ALL);
 date_default_timezone_set("Asia/Bangkok");
 
- $serverName  = "localhost";
- $userName  = "id4708578_test1234";
- $userPassword = "test1234";
- $dbName = "id4708578__ballbib";
-
-
- $mysqli = mysqli_connect($serverName,$userName,$userPassword,$dbName);
-
-	mysqli_set_charset($mysqli, "utf8");
  
+$conn = mysqli_connect("ballbib.000webhostapp.com", "id4708578_test1234","test1234", "id4708578_test1234") or die(mysql_error());
 
 
 
