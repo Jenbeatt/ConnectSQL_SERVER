e02cc1c3eda12b4ac682e280f566fc17d6e4e8af
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2018-04-02 23:14:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn = mysqli_connect("https://databases-auth.000webhost.com/","id4708578_test1234","test1234","id4708578__ballbib");
+$conn = mysqli_connect("https://databases-auth.000webhost.com","id4708578_test1234","test1234","id4708578__ballbib");
 if(!$conn){
 	echo "Not Connnect DatatBase";
 }else{
